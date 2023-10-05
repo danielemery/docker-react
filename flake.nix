98e26e8053f00b0c56db879c49b57ed6405cd95e
Daniel Emery <danielremery@gmail.com>
2023-10-05 17:30:01 +0200

    #3 Add development flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Docker React Development Environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: genAttrs supportedSystems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs-18_x
+            ];
+          };
+        });
+    };
+}
